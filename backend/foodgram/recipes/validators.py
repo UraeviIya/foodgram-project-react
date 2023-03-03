cdc4cafcf09264205c8ec26b3185f856c9b9d296
UraeviIya <97128426+UraeviIya@users.noreply.github.com>
2023-03-03 12:33:25 +0300

    Delete backend directory
    исправление директорий в локально репазитории

--- backend/foodgram/recipes/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def color_validator(color):
-    if color[0] != '#':
-        raise ValidationError("Шестнадцатиричный код цвета начинается с '#'.")
-    return color
