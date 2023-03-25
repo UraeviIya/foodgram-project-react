8d3664f0165a81762eacc920296677adbcf5af52
Илья Ураев <uraevilya@yandex.ru>
2023-03-25 10:57:22 +0300

    добавил путь для статики

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if DEBUG:
-    STATICFILES_DIRS = ['backend_static']
+    STATICFILES_DIRS = ['static']
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
